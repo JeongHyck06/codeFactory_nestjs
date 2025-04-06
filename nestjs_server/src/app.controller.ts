419a76136aa59858acb62bd87d8ce5f579f13167
Jack <devjack050700@gmail.com>
2025-04-07 00:54:37 +0900

    NestJS Hello World 해보기

---
+++ nestjs_server/src/app.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHome() {
+    return 'home page';
+  }
+  @Get('post')
+  getPost() {
+    return 'post Page';
+  }
+
+  @Get('user')
+  getUser() {
+    return 'user Page';
+  }
+}
