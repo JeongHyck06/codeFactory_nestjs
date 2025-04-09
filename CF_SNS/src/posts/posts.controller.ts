2fd131a0e706bacd799248099e9ed72dfd4dda96
Jack <devjack050700@gmail.com>
2025-04-09 16:43:04 +0900

    Get Posts 구현하기

--- CF_SNS/src/posts/posts.controller.ts
+++ CF_SNS/src/posts/posts.controller.ts
@@ -1,24 +1,46 @@
 import { Controller, Get } from '@nestjs/common';
 import { PostsService } from './posts.service';
 
-interface Post {
+interface PostModel {
+  id: number;
   author: string;
   title: string;
   content: string;
   likeCount: number;
   commentCount: number;
 }
+
+let posts: PostModel[] = [
+  {
+    id: 1,
+    author: 'newjeans_official',
+    title: '뉴진스',
+    content: '메이크업 고치고 있는 민지',
+    likeCount: 100000,
+    commentCount: 999999,
+  },
+  {
+    id: 2,
+    author: 'newjeans_official',
+    title: '뉴진스',
+    content: '노래 연습하고 있는 민지',
+    likeCount: 100000,
+    commentCount: 999999,
+  },
+  {
+    id: 3,
+    author: 'newjeans_official',
+    title: '뉴진스',
+    content: '노래 연습하고 있는 다니엘',
+    likeCount: 100000,
+    commentCount: 999999,
+  },
+];
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
   @Get()
-  getPost(): Post {
-    return {
-      author: 'newjeans_official',
-      title: '뉴진스',
-      content: '메이크업 고치고 있는 민지',
-      likeCount: 100000,
-      commentCount: 999999,
-    };
+  getPost() {
+    return posts;
   }
 }
